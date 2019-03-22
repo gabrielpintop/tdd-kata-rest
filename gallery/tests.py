b12f6de1ec7aa0bdff4cf181d7c076d37b859caa
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-03-21 23:24:03 -0500

    Get image list status test (Red)

--- gallery/tests.py
+++ gallery/tests.py
@@ -6,4 +6,9 @@ from .models import Image
 import json
 
 # Create your tests here.
-# class GalleryTestCase(TestCase):
+class GalleryTestCase(TestCase):
+
+    def test_list_images_status(self):
+        url = '/gallery/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
